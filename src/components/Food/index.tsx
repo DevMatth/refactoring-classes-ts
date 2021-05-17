9963bd5ef8a0d00aa46df22a29f0d47fa1c47424
Matheus Leal <lealmirandamts@gmail.com>
2021-05-17 18:06:15 -0300

    commit finaly
    conversão da aplicação concluída.

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -1,40 +1,39 @@
-import { Component } from 'react';
-import { FiEdit3, FiTrash } from 'react-icons/fi';
+import { useState } from 'react';
 
-import { Container } from './styles';
+import { FiEdit3, FiTrash } from 'react-icons/fi';
 import api from '../../services/api';
 
-class Food extends Component {
-  constructor(props) {
-    super(props);
+import { Container } from './styles';
 
-    const { available } = this.props.food;
-    this.state = {
-      isAvailable: available
-    };
-  }
+interface FoodPlate {
+  id: number;
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+  available: boolean;
+}
 
-  toggleAvailable = async () => {
-    const { food } = this.props;
-    const { isAvailable } = this.state;
+type FoodProps = {
+  food: FoodPlate;
+  handleDelete: (id: number) => {};
+  handleEditFood: (food: FoodPlate) => void;
+}
 
+export function Food({ food, handleDelete, handleEditFood }: FoodProps) {
+  const [isAvailable, setIsAvailable] = useState(food.available);
+
+  async function toggleAvailable(): Promise<void> {
     await api.put(`/foods/${food.id}`, {
       ...food,
       available: !isAvailable,
-    });
-
-    this.setState({ isAvailable: !isAvailable });
+    })
+    setIsAvailable(!isAvailable)
   }
 
-  setEditingFood = () => {
-    const { food, handleEditFood } = this.props;
-
+  function setEditingFood(): void {
     handleEditFood(food);
   }
-
-  render() {
-    const { isAvailable } = this.state;
-    const { food, handleDelete } = this.props;
 
     return (
       <Container available={isAvailable}>
@@ -53,7 +52,7 @@ class Food extends Component {
             <button
               type="button"
               className="icon"
-              onClick={this.setEditingFood}
+              onClick={setEditingFood}
               data-testid={`edit-food-${food.id}`}
             >
               <FiEdit3 size={20} />
@@ -77,7 +76,7 @@ class Food extends Component {
                 id={`available-switch-${food.id}`}
                 type="checkbox"
                 checked={isAvailable}
-                onChange={this.toggleAvailable}
+                onChange={toggleAvailable}
                 data-testid={`change-status-food-${food.id}`}
               />
               <span className="slider" />
@@ -86,7 +85,4 @@ class Food extends Component {
         </section>
       </Container>
     );
-  }
 };
-
-export default Food;
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,125 @@
+import { useEffect, useState } from 'react';
+import {Header} from '../../components/Header';
+import {Food} from '../../components/Food';
+import { ModalAddFood } from '../../components/ModalAddFood';
+import { ModalEditFood } from '../../components/ModalEditFood';
+import api from '../../services/api';
+
+import { FoodsContainer } from './styles';
+
+interface FoodPlate {
+  id: number;
+  name: string;
+  image: string;
+  price: string;
+  description: string;
+  available: boolean;
+}
+
+export function Dashboard() {
+  const [foods, setFoods] = useState<FoodPlate[]>([]);
+  const [editingFood , setEditingFood] = useState<FoodPlate>({} as FoodPlate);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+    
+
+  useEffect(() => {
+    async function loadFoods(): Promise<void> {
+      const foodsList = await api.get('/foods')
+
+      setFoods(foodsList.data)
+    }
+
+    loadFoods();
+  }, [])
+
+
+  async function handleAddFood(
+    food: Omit<FoodPlate, 'id' | 'available'>,
+  ): Promise<void> {
+    try {
+      const newFood = {
+        id: foods[foods.length - 1] ? foods[foods.length - 1].id + 1 : 1, 
+        name: food.name,
+        description: food.description,
+        price: food.price,
+        available: true,
+        image: food.image,
+      }
+      await api.post('/foods', newFood);
+      setFoods([...foods, newFood]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+  async function handleUpdateFood(
+    food: Omit<FoodPlate, 'id' | 'available'>,
+  ): Promise<void> {
+    const newFoodList = foods.map(currentFood => {
+      if(currentFood.id !== editingFood.id) {
+        return currentFood;
+      }
+      return {
+        ...food,
+        id: editingFood.id,
+        available: editingFood.available
+      };
+    });
+    setFoods(newFoodList);
+    await api.put(`/foods/${editingFood.id}`,{
+      ...food,
+      id: editingFood.id,
+      available: editingFood.available
+    })
+  }
+
+  async function handleDeleteFood(id: number): Promise<void> {
+    await api.delete(`/foods/${id}`);
+    const newFoodList = foods.filter(currentFood => currentFood.id !== id);
+    setFoods(newFoodList)
+  }
+
+  function toggleModal(): void {
+    setModalOpen(!modalOpen);
+  }
+
+  function toggleEditModal(): void {
+    setEditModalOpen(!editModalOpen);
+  }
+
+  function handleEditFood(food: FoodPlate): void {
+    setEditingFood(food);
+    toggleEditModal();
+  }
+
+    return (
+      <>
+        <Header openModal={toggleModal} />
+        <ModalAddFood
+          isOpen={modalOpen}
+          setIsOpen={toggleModal}
+          handleAddFood={handleAddFood}
+        />
+        <ModalEditFood
+          isOpen={editModalOpen}
+          setIsOpen={toggleEditModal}
+          editingFood={editingFood}
+          handleUpdateFood={handleUpdateFood}
+        />
+
+        <FoodsContainer data-testid="foods-list">
+          {foods &&
+            foods.map(food => (
+              <Food
+                key={food.id}
+                food={food}
+                handleDelete={handleDeleteFood}
+                handleEditFood={handleEditFood}
+              />
+            ))}
+        </FoodsContainer>
+      </>
+    );
+  
+};
